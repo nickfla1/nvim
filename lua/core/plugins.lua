66b0c3cdb20463b1e873db95b8872bd0d9fb8b0d
nickfla1 <lanterniniflavio@gmail.com>
2023-09-20 18:43:17 +0200

    migrated to lazy

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,52 +1,49 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-	use 'ellisonleao/gruvbox.nvim'
-	use 'nvim-tree/nvim-tree.lua'
-	use 'nvim-tree/nvim-web-devicons'
-	use 'nvim-lualine/lualine.nvim'
-	use 'nvim-treesitter/nvim-treesitter'
-	use {
+local plugins = {
+	'ellisonleao/gruvbox.nvim',
+	'nvim-tree/nvim-tree.lua',
+	'nvim-tree/nvim-web-devicons',
+	'nvim-lualine/lualine.nvim',
+	'nvim-treesitter/nvim-treesitter',
+	{
 		'nvim-telescope/telescope.nvim',
 		tag = '0.1.0',
-		requires = { {'nvim-lua/plenary.nvim'} },
-	}
+		dependencies = { {'nvim-lua/plenary.nvim'} },
+	},
 
 	-- binary management
-	use {
+	{
 		'williamboman/mason.nvim',
 		'williamboman/mason-lspconfig.nvim',
 		'neovim/nvim-lspconfig',
 		'saadparwaiz1/cmp_luasnip',
-		'rafamadriz/friendly-snippets,'
-	}
+		'rafamadriz/friendly-snippets'
+	},
 
 	-- code completion
-	use {
+	{
 		'hrsh7th/nvim-cmp',
 		'hrsh7th/cmp-nvim-lsp',
 		'L3MON4D3/LuaSnip',
-	}
+	},
 
 	-- rust
-	use 'simrat39/rust-tools.nvim'
+	'simrat39/rust-tools.nvim',
+}
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+local opts = {}
+
+require("lazy").setup(plugins, opts)
 
